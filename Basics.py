c02a876d0e088d36956c739276329eb718995f9a
Hudson Bean <hudsonbeandev@gmail.com>
2023-10-20 10:22:34 -0500

    Added vector*scalar method for vector class

--- Basics.py
+++ Basics.py
@@ -1,11 +1,6 @@
 # Imports
 import math
 
-a = 1
-b = 2
-c = "Hello World!"
-d = [a, b, c]
-
 def caculate(x : int, y : int, method : str):
     if method == "add":
         return x+y
@@ -20,14 +15,22 @@ def caculate(x : int, y : int, method : str):
     else:
         return "Sorry the method you provided is invalid! Please try again."
 
-
-
 class Vector():
     def __init__(self, x : int, y : int):
         self.direction = {'x': x, 'y': y}
         self.magnitude = math.sqrt(((x*x) + (y*y)))
-    def __call__(self):
+    def info(self):
         print("Your vector is:\n" + "Direction: <" + str(self.direction['x']) + ", " + str(self.direction['y']) + ">" + "\nMagnitude: " + str(self.magnitude))
+    def __mul__(self, a):
+        if (type(a) is int): # Caculates for scalar
+            x = self.direction['x'] * a
+            y = self.direction['y'] * a
+            d = {'x': x, 'y': y}
+            m = math.sqrt(((x*x) + (y*y)))
+            return {"direction": d, "magnitude": m}
+        
+        elif (type(a) is Vector):
+            print("YES!")
+    
         
 v = Vector(3, 4)
-v()
