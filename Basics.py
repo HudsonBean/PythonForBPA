c635d195c75d8d8f6e9c4d190b0542f536913cf4
Hudson Bean <hudsonbeandev@gmail.com>
2023-10-20 09:59:25 -0500

    Changed name made vector class

--- Basics.py
+++ Basics.py
@@ -1,15 +1,12 @@
+# Imports
+import math
+
 a = 1
 b = 2
 c = "Hello World!"
 d = [a, b, c]
 
-print("We have 4 variables!")
-print("The value of variable a is: " + str(a))
-print("The value of variable b is: " + str(b))
-print("The value of variable c is: " + c)
-print("The value of variable c is: " + str(d))
-
-def caculate(x, y, method):
+def caculate(x : int, y : int, method : str):
     if method == "add":
         return x+y
     elif method == "subtract":
@@ -22,4 +19,14 @@ def caculate(x, y, method):
         return x**y
     else:
         return "Sorry the method you provided is invalid! Please try again."
-print(str(caculate(a,b,"ada")))
+
+
+
+class Vector():
+    def __init__(self, x : int, y : int):
+        self.x = x
+        self.y = y
+        self.m = math.sqrt((x**x) + (y**y))
+        
+v = Vector(3, 4)
+print(v.m)
