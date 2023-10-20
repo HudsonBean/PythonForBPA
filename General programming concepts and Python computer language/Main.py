f2e905382055373635e9b2529defa7c84f9ab09c
Hudson Bean <hudsonbeandev@gmail.com>
2023-10-20 08:39:54 -0500

    Added some variables, some prints, some functions, etc.

--- General programming concepts and Python computer language/Main.py
+++ General programming concepts and Python computer language/Main.py
@@ -1,1 +1,25 @@
-print("Hello World!")
+a = 1
+b = 2
+c = "Hello World!"
+d = [a, b, c]
+
+print("We have 4 variables!")
+print("The value of variable a is: " + str(a))
+print("The value of variable b is: " + str(b))
+print("The value of variable c is: " + c)
+print("The value of variable c is: " + str(d))
+
+def caculate(x, y, method):
+    if method == "add":
+        return x+y
+    elif method == "subtract":
+        return x-y
+    elif method == "multiply":
+        return x*y
+    elif method == "divide":
+        return x/y
+    elif method == "raise":
+        return x**y
+    else:
+        return "Sorry the method you provided is invalid! Please try again."
+print(str(caculate(a,b,"ada")))
