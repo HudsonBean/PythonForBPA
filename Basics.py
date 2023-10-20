f5e480ae51907a03a190658ed2d2dc9b73c9a5b5
Hudson Bean <hudsonbeandev@gmail.com>
2023-10-20 16:15:35 -0500

    Added dot product

--- Basics.py
+++ Basics.py
@@ -1,6 +1,7 @@
 # Imports
 import math
 
+
 def caculate(x : int, y : int, method : str):
     if method == "add":
         return x+y
@@ -19,37 +20,36 @@ class Vector():
     def __init__(self, x : int, y : int):
         self.direction = {'x': x, 'y': y}
         self.magnitude = math.sqrt(((x*x) + (y*y)))
-    def info(self):
-        print("Your vector is:\n" + "Direction: <" + str(self.direction['x']) + ", " + str(self.direction['y']) + ">" + "\nMagnitude: " + str(self.magnitude))
+    def __cv(self, x, y):
+        d = {'x': x, 'y': y}
+        m = math.sqrt(((x*x) + (y*y)))
+        return {"direction": d, "magnitude": m}
     def __mul__(self, a):
         if (type(a) is int): # Caculates for scalar
             x = self.direction['x'] * a
             y = self.direction['y'] * a
-            d = {'x': x, 'y': y}
-            m = math.sqrt(((x*x) + (y*y)))
-            return {"direction": d, "magnitude": m}
-        
+            return self.__cv(x,y)
         elif (type(a) is Vector):
-            debounce = False
-            while (debounce == False):
-                user_input = input("What method would you like?\n   (1) Cross-Product\n   (2) Dot-Product\n>")
-                if (int(user_input) == 1):
-                    debounce = True
-                    print('Cross')
-                elif (int(user_input) == 2):
-                    debounce = True
-                    # print('Dot')
-                else:
-                    debounce = False
-                    print("The method you inputted was incorrect!\n")
-                    user_input = input("Would you like to retry?\n    (1) Y\n    (2) N\n>")
-                    if (not user_input.isdigit()):
-                        return
-                    if (int(user_input) == 2):
-                        return
-                    else:
-                        print("\n\n\n")
-                        pass
+            print("Attempting to multiply a vector by a vector! Use the .dotProduct or crossProduct function to multiply vectors.")
+            return self
+    def info(self):
+        print("Your vector is:\n" + "Direction: <" + str(self.direction['x']) + ", " + str(self.direction['y']) + ">" + "\nMagnitude: " + str(self.magnitude))
+        aOne = self.direction['x']
+        aTwo = self.direction['y']
+        
+        bOne = v.direction['x']
+        bTwo = v.direction['y']
+        
+        i = (aOne*bTwo)
+        j = ((bOne*aTwo)*-1)
+        
+        return self.__cv(i,j)
+    def dotProduct(self, v):
+        x = self.direction['x']*v.direction['x']
+        y = self.direction['y']*v.direction['y']
+        return x+y
+        
         
 v = Vector(3, 4)
-v * v
+vTwo = Vector(8,1)
+print(v.dotProduct(vTwo))
