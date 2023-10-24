c96068e0327aa3b513ed7c66c8d26fa449592539
Hudson Bean <hudsonbeandev@gmail.com>
2023-10-24 10:53:14 -0500

    # Get dial ammount and combinations ammount

---
+++ PythonLastYearTest.py
@@ -0,0 +1,6 @@
+# Needs 3 dials
+# Each dial has 0-9
+
+# Get dial ammount and combinations ammount
+DIAL_AMMOUNT = int(input("Each lock has how many dials?\n> "))
+COMBINATIONS_AMMOUNT = int(input("How many combinations should I generate?\n> "))
