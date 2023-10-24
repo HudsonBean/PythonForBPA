c4bb665c96e436b4bcab4736bf68feb74f9697bd
Hudson Bean <hudsonbeandev@gmail.com>
2023-10-24 11:36:28 -0500

    Created lock thing

--- PythonLastYearTest.py
+++ PythonLastYearTest.py
@@ -1,6 +1,22 @@
 # Needs 3 dials
 # Each dial has 0-9
 
-# Get dial ammount and combinations ammount
-DIAL_AMMOUNT = int(input("Each lock has how many dials?\n> "))
-COMBINATIONS_AMMOUNT = int(input("How many combinations should I generate?\n> "))
+import random
+from array import *
+
+def getRandomCombination():
+    # Get dial ammount and combinations ammount
+    DIAL_AMMOUNT = int(input("Each lock has how many dials?\n> "))
+    COMBINATIONS_AMMOUNT = int(input("How many combinations should I generate?\n> "))
+    Ans = []
+    i = 0
+    while i < COMBINATIONS_AMMOUNT:
+        a = [] # Create blank table
+        j = 0 # Reset number for loop
+        while j < DIAL_AMMOUNT:
+            list.append(a, random.randrange(0,10))
+            j+=1
+        list.append(Ans, a)
+        i+=1 # Increase for next set of combinations
+    return Ans
+print(getRandomCombination())
